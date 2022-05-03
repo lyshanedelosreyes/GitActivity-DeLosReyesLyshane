a1d9ae4aef89a09a9103cc13b07bb74ab0067837
DeLosReyesLyshane <lyshanedelosreyes@gmail.com>
2022-05-03 10:04:41 +0800

    My Educational Background added

--- git-python-DeLosReyes.py
+++ git-python-DeLosReyes.py
@@ -1,3 +1,6 @@
 print ('Lyshane Ola De Los Reyes')
 print ('Malusak, Boac, Marinduque')
 print ('December 13, 1998')
+print ('Marinduque State College')
+print ('Bachelor of Science in Information Technology')
+print ('2-A')
